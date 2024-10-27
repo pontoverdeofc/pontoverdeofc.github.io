7b091217843516cf1576e7182313b6585a6ec760
Guilherme Chaves <guilherme.clc17@gmail.com>
2024-10-27 16:33:20 -0300

    Reset

--- src/Configurations/firebaseConfig.jsx
+++
@@ -1,9 +0,0 @@
-import { initializeApp } from "firebase/app";
-import { getDatabase } from "firebase/database";
-
-const firebaseConfig = {
-  databaseURL: "https://projeto3-web1-default-rtdb.firebaseio.com/",
-};
-
-const app = initializeApp(firebaseConfig);
-export const db = getDatabase(app);
